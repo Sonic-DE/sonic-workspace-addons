6f989ca7b5d6573a3e4490c8fee9fc61e785fe61
Shaun Reich <shaun.reich@kdemail.net>
2012-01-14 14:31:24 -0500

    rename everything from screensaver-inhibitor to
    powermanagement-inhibitor. since it's going to be calling those methods
    additionally...

--- applets/powermanagement-inhibitor/Messages.sh
+++ applets/powermanagement-inhibitor/Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 # only need to change the name of the applet
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/screensaver-inhibitor.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/powermanagement-inhibitor.pot
 rm -f rc.cpp
 
