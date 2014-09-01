3917d80b059c58edafe6d66a3c8f4c60c11eb9b7
Lukáš Tinkl <lukas@kde.org>
2014-09-01 12:01:09 +0200

    fix Messages targets
    we have to extract (also)from QML files if the applet
    gets ported from CPP to QML

--- applets/notes/Messages.sh
+++ applets/notes/Messages.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/plasma_applet_org.kde.plasma.notes.pot
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_org.kde.plasma.notes.pot
+rm -f rc.cpp
--- applets/quickshare/Messages.sh
+++ applets/quickshare/Messages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp *.h -o $podir/plasma_applet_org.kde.plasma.quickshare.pot
+#! /usr/bin/env bash
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_org.kde.plasma.quickshare.pot
 rm -f rc.cpp
