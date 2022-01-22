6d8ecf824c05075704fca8b927705b29319dd07c
Fushan Wen <qydwhotmail@gmail.com>
2022-01-22 16:43:20 +0800

    dataengines/potd: Parse title, author, infoUrl and remoteUrl in FlickrProvider
    Parse the title from XML, the author from the XML data, and set the
    info URL and the remote URL.
    
    CCBUG: 422934

--- dataengines/potd/flickrprovider.h
+++ dataengines/potd/flickrprovider.h
@@ -53,6 +53,11 @@ private:
     void xmlRequestFinished(KJob *job);
     void imageRequestFinished(KJob *job);
 
+    /**
+     * Parse the author from the HTML source
+     */
+    void pageRequestFinished(KJob *job);
+
 private:
     QDate mActualDate;
     QString mApiKey;
@@ -62,7 +67,13 @@ private:
 
     int mFailureNumber = 0;
 
-    QStringList m_photoList;
+    struct PhotoEntry {
+        QString urlString;
+        QString title;
+        QString userId;
+        QString photoId;
+    };
+    std::vector<PhotoEntry> m_photoList;
 };
 
 #endif
