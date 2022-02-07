cb7c609414440d7b1b7d938ccaf2809b904c2acc
Fushan Wen <qydwhotmail@gmail.com>
2022-02-07 19:35:17 +0800

    alternatecalendar: Add plugin base
    This adds the basic structure of the alternate calendar plugin.
    
    CCBUG: 429892

---
+++ plasmacalendarplugins/alternatecalendar/alternatecalendarplugin.h
@@ -0,0 +1,57 @@
+/*
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#ifndef ALTERNATECALENDARPLUGIN_H
+#define ALTERNATECALENDARPLUGIN_H
+
+#include <memory>
+
+#include <KConfigWatcher>
+
+#include <CalendarEvents/CalendarEventsPlugin>
+
+#include "calendarsystem.h"
+
+class KConfigGroup;
+
+namespace AlternateCalendarPlugin
+{
+
+class AlternateCalendarPlugin;
+class AlternateCalendarPluginPrivate
+{
+public:
+    explicit AlternateCalendarPluginPrivate(AlternateCalendarPlugin *parent);
+    ~AlternateCalendarPluginPrivate() = default;
+
+    void init();
+    CalendarSystem::System calendarSystem() const;
+
+private:
+    CalendarSystem::System m_calendarSystem;
+
+    AlternateCalendarPlugin *p;
+};
+
+class AlternateCalendarPlugin : public CalendarEvents::CalendarEventsPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.kde.CalendarEventsPlugin" FILE "alternatecalendarplugin.json")
+    Q_INTERFACES(CalendarEvents::CalendarEventsPlugin)
+
+public:
+    AlternateCalendarPlugin();
+    ~AlternateCalendarPlugin() override = default;
+
+    void loadEventsForDateRange(const QDate &startDate, const QDate &endDate) override;
+
+private:
+    std::unique_ptr<AlternateCalendarPluginPrivate> d;
+};
+
+}
+
+#endif
