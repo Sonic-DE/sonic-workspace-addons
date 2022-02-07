8b8547a287ce02ab95e1af3ebaac524d44a22bf9
Fushan Wen <qydwhotmail@gmail.com>
2022-02-07 19:44:57 +0800

    alternatecalendar: Add QCalendar provider
    This implements calendar system supports from QCalendar.
    
    CCBUG: 429892

---
+++ plasmacalendarplugins/alternatecalendar/provider/qtcalendar.h
@@ -0,0 +1,38 @@
+/*
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "abstractcalendarprovider.h"
+
+namespace AlternateCalendarPlugin
+{
+
+class QtCalendarProviderPrivate;
+
+/**
+ * @short An alternate calendar provider for the calendar system available in QCalendar.
+ *
+ * This class presents an alternate calendar provider for the calendar system available
+ * in QCalendar.
+ * The available calendar systems are listed in QCalendar::availableCalendars().
+ */
+class QtCalendarProvider : public AbstractCalendarProvider
+{
+    Q_OBJECT
+
+public:
+    explicit QtCalendarProvider(CalendarSystem::System calendarSystem);
+    ~QtCalendarProvider() override;
+
+    QDate fromGregorian(const QDate &date) const override;
+    CalendarEvents::CalendarEventsPlugin::SubLabel subLabels(const QDate &date) const override;
+
+private:
+    std::unique_ptr<QtCalendarProviderPrivate> d;
+};
+
+}
