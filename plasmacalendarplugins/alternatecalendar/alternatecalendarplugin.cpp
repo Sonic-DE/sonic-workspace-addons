fe68a27c36083cf4841cca60b3b40cf3f6fa2ca3
Fushan Wen <qydwhotmail@gmail.com>
2022-02-07 19:52:54 +0800

    alternatecalendar: Add support for QCalendar provider in plugin base
    Supported calendar systems are from QCalendar.
    
    CCBUG: 429892

--- plasmacalendarplugins/alternatecalendar/alternatecalendarplugin.cpp
+++ plasmacalendarplugins/alternatecalendar/alternatecalendarplugin.cpp
@@ -41,6 +41,18 @@ void AlternateCalendarPluginPrivate::init()
 {
     // Load/Reload the calendar provider
     switch (m_calendarSystem) {
+#ifndef QT_BOOTSTRAPPED
+    case CalendarSystem::Julian:
+    case CalendarSystem::Milankovic:
+#endif
+#if QT_CONFIG(jalalicalendar)
+    case CalendarSystem::Jalali:
+#endif
+#if QT_CONFIG(islamiccivilcalendar)
+    case CalendarSystem::IslamicCivil:
+#endif
+        m_calendarProvider.reset(new QtCalendarProvider(p, m_calendarSystem));
+        break;
     default:
         m_calendarProvider.reset(new AbstractCalendarProvider(p, m_calendarSystem));
     }
