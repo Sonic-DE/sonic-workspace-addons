9fa0cc1bedde500d94ea33dc02ba7ae7168c32b9
Fushan Wen <qydwhotmail@gmail.com>
2022-02-07 20:27:35 +0800

    alternatecalendar: Add Indian national calendar
    The Indian national calendar, sometimes called the Saka calendar, is
    used, alongside the Gregorian calendar, by The Gazette of India, in
    news broadcasts by All India Radio and in calendars and communications
    issued by the Government of India.
    
    CCBUG: 429892

---
+++ plasmacalendarplugins/alternatecalendar/provider/indiancalendar.cpp
@@ -0,0 +1,78 @@
+/*
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "indiancalendar.h"
+
+namespace AlternateCalendarPlugin
+{
+
+class IndianCalendarProviderPrivate : public ICUCalendarPrivate
+{
+public:
+    explicit IndianCalendarProviderPrivate();
+    ~IndianCalendarProviderPrivate() override;
+
+    QDate fromGregorian(const QDate &_date);
+    CalendarEvents::CalendarEventsPlugin::SubLabel subLabels(const QDate &date);
+};
+
+IndianCalendarProviderPrivate::IndianCalendarProviderPrivate()
+    : ICUCalendarPrivate()
+{
+    UErrorCode errorCode = U_ZERO_ERROR;
+    m_calendar.reset(icu::Calendar::createInstance("en_US@calendar=indian", errorCode));
+    Q_ASSERT(!U_FAILURE(errorCode));
+}
+
+IndianCalendarProviderPrivate::~IndianCalendarProviderPrivate()
+{
+}
+
+QDate IndianCalendarProviderPrivate::fromGregorian(const QDate &_date)
+{
+    if (!_date.isValid() || !setDate(_date)) {
+        return QDate();
+    }
+
+    return date();
+}
+
+CalendarEvents::CalendarEventsPlugin::SubLabel IndianCalendarProviderPrivate::subLabels(const QDate &date)
+{
+    if (!date.isValid() || !setDate(date)) {
+        return CalendarEvents::CalendarEventsPlugin::SubLabel{};
+    }
+
+    return CalendarEvents::CalendarEventsPlugin::SubLabel{
+        QString::number(year()),
+        QString::number(month()),
+        QString::number(day()),
+        CalendarEvents::CalendarEventsPlugin::SubLabelPriority::Low,
+    };
+}
+
+IndianCalendarProvider::IndianCalendarProvider(CalendarSystem::System calendarSystem)
+    : AbstractCalendarProvider(calendarSystem)
+    , d(new IndianCalendarProviderPrivate())
+{
+    Q_ASSERT(m_calendarSystem == CalendarSystem::Indian);
+}
+
+IndianCalendarProvider::~IndianCalendarProvider()
+{
+}
+
+QDate IndianCalendarProvider::fromGregorian(const QDate &date) const
+{
+    return d->fromGregorian(date);
+}
+
+CalendarEvents::CalendarEventsPlugin::SubLabel IndianCalendarProvider::subLabels(const QDate &date) const
+{
+    return d->subLabels(date);
+}
+
+}
