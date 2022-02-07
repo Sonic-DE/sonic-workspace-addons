6af383ed647ba747a36506cd35f70537a8c9eb65
Fushan Wen <qydwhotmail@gmail.com>
2022-02-07 19:39:21 +0800

    alternatecalendar: Add abstract calendar provider class
    This base class will be used in alternate calendar provider
    implementations.
    
    CCBUG: 429892

---
+++ plasmacalendarplugins/alternatecalendar/provider/abstractcalendarprovider.cpp
@@ -0,0 +1,35 @@
+/*
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "abstractcalendarprovider.h"
+
+namespace AlternateCalendarPlugin
+{
+
+AbstractCalendarProvider::AbstractCalendarProvider(CalendarSystem::System calendarSystem)
+    : m_calendarSystem(calendarSystem)
+{
+}
+
+AbstractCalendarProvider::~AbstractCalendarProvider()
+{
+}
+
+QDate AbstractCalendarProvider::fromGregorian(const QDate &date) const
+{
+    if (!date.isValid()) {
+        return QDate();
+    }
+
+    return date;
+}
+
+CalendarEvents::CalendarEventsPlugin::SubLabel AbstractCalendarProvider::subLabels([[maybe_unused]] const QDate &date) const
+{
+    return CalendarEvents::CalendarEventsPlugin::SubLabel{QString(), QString(), QString(), CalendarEvents::CalendarEventsPlugin::SubLabelPriority::Low};
+}
+
+}
