c23c978a799e286671bfadff30cb9f5f2e621538
Fushan Wen <qydwhotmail@gmail.com>
2022-02-07 19:35:17 +0800

    alternatecalendar: Add plugin base
    This adds the basic structure of the alternate calendar plugin.
    
    CCBUG: 429892

---
+++ plasmacalendarplugins/alternatecalendar/alternatecalendarplugin.cpp
@@ -0,0 +1,53 @@
+/*
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "alternatecalendarplugin.h"
+
+#include <KConfigGroup>
+#include <KSharedConfig>
+
+namespace AlternateCalendarPlugin
+{
+
+AlternateCalendarPluginPrivate::AlternateCalendarPluginPrivate(AlternateCalendarPlugin *parent)
+    : p(parent)
+{
+}
+
+void AlternateCalendarPluginPrivate::init()
+{
+}
+
+CalendarSystem::System AlternateCalendarPluginPrivate::calendarSystem() const
+{
+    return m_calendarSystem;
+}
+
+AlternateCalendarPlugin::AlternateCalendarPlugin()
+    : CalendarEvents::CalendarEventsPlugin()
+    , d(new AlternateCalendarPluginPrivate(this))
+{
+}
+
+void AlternateCalendarPlugin::loadEventsForDateRange(const QDate &startDate, const QDate &endDate)
+{
+    QHash<QDate, QDate> alternateDatesData;
+    QHash<QDate, SubLabel> subLabelsData;
+
+    if (!endDate.isValid() || d->calendarSystem() == CalendarSystem::Gregorian) {
+        return;
+    }
+
+    for (QDate date = startDate; date <= endDate && date.isValid(); date = date.addDays(1)) {
+    }
+
+    if (alternateDatesData.size() > 0) {
+        Q_EMIT alternateDateReady(alternateDatesData);
+    }
+    Q_EMIT subLabelReady(subLabelsData);
+}
+
+}
