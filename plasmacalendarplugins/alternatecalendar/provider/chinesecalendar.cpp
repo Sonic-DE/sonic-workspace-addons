949260f05e3ef18af3d3c8ec71efee15d2fc9fb0
Fushan Wen <qydwhotmail@gmail.com>
2022-02-07 20:24:31 +0800

    alternatecalendar: Add Chinese lunar calendar system support
    The traditional Chinese calendar (also known as the Agricultural
    Calendar is a lunisolar calendar which identifies years, months,
    and days according to astronomical phenomena. In China, it is defined
    by the Chinese national standard GB/T 33661–2017, "Calculation and
    Promulgation of the Chinese Calendar", issued by the Standardization
    Administration of China on May 12, 2017.
    
    The plugin is adapted from https://github.com/BLumia/pineapple-calendar
    
    The original author: Gary Wang <wzc782970009@gmail.com>
    
    FEATURE: 429892

---
+++ plasmacalendarplugins/alternatecalendar/provider/chinesecalendar.cpp
@@ -0,0 +1,107 @@
+/*
+    SPDX-FileCopyrightText: 2021 Gary Wang <wzc782970009@gmail.com>
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "chinesecalendar.h"
+
+namespace AlternateCalendarPlugin
+{
+
+class ChineseCalendarProviderPrivate : public ICUCalendarPrivate
+{
+public:
+    explicit ChineseCalendarProviderPrivate();
+    ~ChineseCalendarProviderPrivate() override;
+
+    CalendarEvents::CalendarEventsPlugin::SubLabel subLabels(const QDate &date);
+
+private:
+    /**
+     * For formatting, see the documentation of SimpleDateFormat:
+     * https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classicu_1_1SimpleDateFormat.html#details
+     */
+    QString formattedDateString(const icu::UnicodeString &str, bool hanidays = false) const;
+
+    QString yearDisplayName() const;
+    QString monthDisplayName() const;
+    QString dayDisplayName() const;
+};
+
+ChineseCalendarProviderPrivate::ChineseCalendarProviderPrivate()
+    : ICUCalendarPrivate()
+{
+    if (U_FAILURE(m_errorCode)) {
+        return; // Failed to create m_GregorianCalendar
+    }
+
+    m_calendar.reset(icu::Calendar::createInstance("en_US@calendar=chinese", m_errorCode));
+}
+
+ChineseCalendarProviderPrivate::~ChineseCalendarProviderPrivate()
+{
+}
+
+CalendarEvents::CalendarEventsPlugin::SubLabel ChineseCalendarProviderPrivate::subLabels(const QDate &date)
+{
+    if (U_FAILURE(m_errorCode) || !date.isValid() || !setDate(date)) {
+        return CalendarEvents::CalendarEventsPlugin::SubLabel{};
+    }
+
+    return CalendarEvents::CalendarEventsPlugin::SubLabel{
+        yearDisplayName(),
+        monthDisplayName(),
+        day() == 1 ? monthDisplayName() : dayDisplayName(),
+        CalendarEvents::CalendarEventsPlugin::SubLabelPriority::Low,
+    };
+}
+
+QString ChineseCalendarProviderPrivate::formattedDateString(const icu::UnicodeString &str, bool hanidays) const
+{
+    const icu::Locale locale("zh", 0, 0, hanidays ? "calendar=chinese;numbers=hanidays" : "calendar=chinese");
+    UErrorCode errorCode = U_ZERO_ERROR;
+    icu::UnicodeString dateString;
+    icu::SimpleDateFormat formatter(str, locale, errorCode);
+    formatter.setCalendar(*m_calendar);
+    formatter.format(m_calendar->getTime(errorCode), dateString);
+
+    std::string utf8Str;
+    dateString.toUTF8String<std::string>(utf8Str);
+
+    return QString::fromUtf8(utf8Str.c_str());
+}
+
+QString ChineseCalendarProviderPrivate::yearDisplayName() const
+{
+    return formattedDateString("U");
+}
+
+QString ChineseCalendarProviderPrivate::monthDisplayName() const
+{
+    return formattedDateString("MMM");
+}
+
+QString ChineseCalendarProviderPrivate::dayDisplayName() const
+{
+    return formattedDateString("d", true);
+}
+
+ChineseCalendarProvider::ChineseCalendarProvider(QObject *parent, CalendarSystem::System calendarSystem)
+    : AbstractCalendarProvider(parent, calendarSystem)
+    , d(new ChineseCalendarProviderPrivate())
+{
+    Q_ASSERT(m_calendarSystem == CalendarSystem::Chinese);
+}
+
+ChineseCalendarProvider::~ChineseCalendarProvider()
+{
+}
+
+CalendarEvents::CalendarEventsPlugin::SubLabel ChineseCalendarProvider::subLabels(const QDate &date) const
+{
+    return d->subLabels(date);
+}
+
+}
