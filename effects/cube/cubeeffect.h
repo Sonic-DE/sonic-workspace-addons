20aa5630d0cf92da6bdbf100f9162c6316887e99
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2022-07-04 17:54:07 +0300

    effects/cube: New effect
    This change implements the desktop cube effect using QtQuick 3D. It's a
    much simpler version of the old desktop cube effect. You can rotate the
    cube and click LMB to switch to the currently viewed virtual desktop.
    
    Keyboard navigation is simple too:
    
    - `Escape` - quit the effect
    - `Left` and `Right` arrow keys - rotate the cube left or right, respectively
    - `Enter`/`Space`/`Return` - switch to the currently viewed desktop
    
    The cube effect reserves `Meta+C` keyboard shortcut. You need to have at
    least three virtual desktops in order to activate the effect.
    
    The effect is licensed under GPLv3-only because
    OrbitCameraController.qml was copied from qtquick3d repo, where most of
    the stuff is under LicenseRef-Qt-Commercial OR GPL-3.0-only. I made
    extra additions in OrbitCameraController.qml to limit rotations in the x axis.
    
    BUG: 438883

---
+++ effects/cube/cubeeffect.h
@@ -0,0 +1,78 @@
+/*
+    SPDX-FileCopyrightText: 2022 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-3.0-only
+*/
+
+#pragma once
+
+#include <kwinquickeffect.h>
+
+#include <QKeySequence>
+
+namespace KWin
+{
+
+class CubeEffect : public QuickSceneEffect
+{
+    Q_OBJECT
+    Q_PROPERTY(int animationDuration READ animationDuration NOTIFY animationDurationChanged)
+    Q_PROPERTY(qreal cubeFaceDisplacement READ cubeFaceDisplacement NOTIFY cubeFaceDisplacementChanged)
+    Q_PROPERTY(qreal distanceFactor READ distanceFactor NOTIFY distanceFactorChanged)
+    Q_PROPERTY(bool mouseInvertedX READ mouseInvertedX NOTIFY mouseInvertedXChanged)
+    Q_PROPERTY(bool mouseInvertedY READ mouseInvertedY NOTIFY mouseInvertedYChanged)
+
+public:
+    CubeEffect();
+
+    void reconfigure(ReconfigureFlags flags) override;
+    int requestedEffectChainPosition() const override;
+    void grabbedKeyboardEvent(QKeyEvent *e) override;
+    bool borderActivated(ElectricBorder border) override;
+
+    int animationDuration() const;
+    void setAnimationDuration(int duration);
+
+    qreal cubeFaceDisplacement() const;
+    void setCubeFaceDisplacement(qreal displacement);
+
+    qreal distanceFactor() const;
+    void setDistanceFactor(qreal factor);
+
+    bool mouseInvertedX() const;
+    void setMouseInvertedX(bool inverted);
+
+    bool mouseInvertedY() const;
+    void setMouseInvertedY(bool inverted);
+
+public Q_SLOTS:
+    void activate();
+    void deactivate();
+    void toggle();
+
+Q_SIGNALS:
+    void cubeFaceDisplacementChanged();
+    void distanceFactorChanged();
+    void mouseInvertedXChanged();
+    void mouseInvertedYChanged();
+    void animationDurationChanged();
+
+protected:
+    QVariantMap initialProperties(EffectScreen *screen) override;
+
+private:
+    void realDeactivate();
+
+    QTimer *m_shutdownTimer;
+    QAction *m_toggleAction = nullptr;
+    QList<QKeySequence> m_toggleShortcut;
+    QList<ElectricBorder> m_borderActivate;
+    QList<ElectricBorder> m_touchBorderActivate;
+    qreal m_cubeFaceDisplacement = 100;
+    qreal m_distanceFactor = 1.5;
+    int m_animationDuration = 200;
+    bool m_mouseInvertedX = true;
+    bool m_mouseInvertedY = true;
+};
+
+} // namespace KWin
---
+++ effects/cube/qml/Cube.qml
@@ -0,0 +1,55 @@
+/*
+    SPDX-FileCopyrightText: 2022 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-3.0-only
+*/
+
+import QtQuick 2.15
+import QtQuick3D 1.15
+import org.kde.kwin 3.0 as KWinComponents
+
+Node {
+    id: cube
+
+    property real faceDisplacement: 100
+    required property size faceSize
+    readonly property real faceDistance: 0.5 * faceSize.width / Math.tan(baseAngle * Math.PI / 360) + faceDisplacement;
+    readonly property real baseAngle: 360 / faceRepeater.count
+    readonly property QtObject selectedDesktop: {
+        let index = Math.round(eulerRotation.y / baseAngle) % faceRepeater.count;
+        if (index < 0) {
+            index += faceRepeater.count;
+        }
+        return faceRepeater.objectAt(index).desktop;
+    }
+
+    function rotateToLeft() {
+        const index = selectedDesktop.x11DesktopNumber - 1;
+        cube.rotation = Quaternion.fromEulerAngles(0, cube.baseAngle * (index + 1), 0);
+    }
+
+    function rotateToRight() {
+        const index = selectedDesktop.x11DesktopNumber - 1;
+        cube.rotation = Quaternion.fromEulerAngles(0, cube.baseAngle * (index - 1), 0);
+    }
+
+    function rotateTo(desktop) {
+        const index = desktop.x11DesktopNumber - 1;
+        cube.rotation = Quaternion.fromEulerAngles(0, cube.baseAngle * index, 0);
+    }
+
+    Repeater3D {
+        id: faceRepeater
+        model: KWinComponents.VirtualDesktopModel {}
+        delegate: CubeFace {
+            faceSize: cube.faceSize
+            scale: Qt.vector3d(faceSize.width / 100, faceSize.height / 100, 1)
+            eulerRotation.y: -cube.baseAngle * index
+            position: {
+                const transform = Qt.matrix4x4();
+                transform.rotate(-cube.baseAngle * index, Qt.vector3d(0, 1, 0));
+                return transform.times(Qt.vector3d(0, 0, cube.faceDistance));
+            }
+        }
+    }
+}
