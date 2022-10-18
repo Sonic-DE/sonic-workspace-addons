85ffcdc1862a03f553dd793f391df56475f1c32e
Alexander Lohnau <alexander.lohnau@gmx.de>
2022-10-18 20:52:52 +0200

    runners/dictionary: Copy query and text to clipboard when match is run
    FEATURE: 460590
    FIXED-IN: 5.27

--- runners/dictionary/dictionaryrunner.cpp
+++ runners/dictionary/dictionaryrunner.cpp
@@ -7,7 +7,10 @@
 
 #include <KConfigGroup>
 #include <KLocalizedString>
+#include <KNotifications/KNotification>
+#include <QClipboard>
 #include <QEventLoop>
+#include <QGuiApplication>
 #include <QMutex>
 #include <QMutexLocker>
 #include <QStringList>
@@ -104,6 +107,22 @@ void DictionaryRunner::match(RunnerContext &context)
         matches.append(match);
     }
     context.addMatches(matches);
+}
+
+void DictionaryRunner::run(const RunnerContext &context, const QueryMatch &match)
+{
+    QString query = context.query();
+    if (query.startsWith(m_triggerWord, Qt::CaseInsensitive)) {
+        query.remove(0, m_triggerWord.length());
+    }
+    QGuiApplication::clipboard()->setText(query + QLatin1Char(' ') + match.text());
+    KNotification::event(QStringLiteral("copied"),
+                         i18n("Definition for \"%1\" has been copied to clipboard", query),
+                         QString(),
+                         QString(),
+                         nullptr,
+                         KNotification::CloseOnTimeout,
+                         "dictionaryrunner");
 }
 
 K_PLUGIN_CLASS_WITH_JSON(DictionaryRunner, "plasma-runner-dictionary.json")
