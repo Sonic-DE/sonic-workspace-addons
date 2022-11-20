e5054a1e2a9ecae8d10a93e1226af2932e67314e
Fushan Wen <qydwhotmail@gmail.com>
2022-11-20 10:56:18 +0800

    wallpapers/potd: port away from NetworkManagerQt in Qt6
    Prefer QNetworkInformation over NetworkManagerQt since the former is in
    upstream Qt and the latter is likely to become deprecated in the future.

--- wallpapers/potd/plugins/config-NetworkManagerQt.h.cmake
+++ wallpapers/potd/plugins/config-NetworkManagerQt.h.cmake
@@ -1,1 +1,9 @@
 #cmakedefine01 HAVE_NetworkManagerQt
+
+#if HAVE_NetworkManagerQt
+#define SUPPORT_METERED_DETECTION 1
+#elif QT_VERSION >= QT_VERSION_CHECK(6, 3, 0)
+#define SUPPORT_METERED_DETECTION 1
+#else
+#define SUPPORT_METERED_DETECTION 0
+#endif
