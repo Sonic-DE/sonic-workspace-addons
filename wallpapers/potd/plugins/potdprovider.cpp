5e8596ac57b0bd53d9a8294caafc4f580f49aec7
Fushan Wen <qydwhotmail@gmail.com>
2023-02-01 00:55:07 +0800

    wallpapers/potd: port away from deprecated API, simplify engine
    Main changes:
    
    1. Remove QImage in PotdProvider, use local path instead.
    2. Add more virtual functions in PotdProvider
    3. Move Flickr specific code to FlickrProvider

--- wallpapers/potd/plugins/potdprovider.cpp
+++ wallpapers/potd/plugins/potdprovider.cpp
@@ -5,13 +5,7 @@
 #include "potdprovider.h"
 
 #include <QDate>
-#include <QDebug>
-#include <QFileInfo>
-
-#include <KConfig>
-#include <KConfigGroup>
-
-#define CONFIG_ROOT_URL "https://autoconfig.kde.org/potd/"
+#include <QThreadPool>
 
 class PotdProviderPrivate
 {
@@ -22,12 +16,9 @@ public:
     QString name;
     QDate date;
     QString identifier;
-
-    std::unique_ptr<PotdProviderData> m_data;
 };
 
 PotdProviderPrivate::PotdProviderPrivate()
-    : m_data(std::make_unique<PotdProviderData>())
 {
 }
 
@@ -37,44 +28,29 @@ PotdProviderPrivate::~PotdProviderPrivate()
 
 PotdProvider::PotdProvider(QObject *parent, const KPluginMetaData &data, const QVariantList &args)
     : QObject(parent)
-    , d(new PotdProviderPrivate)
+    , d_ptr(new PotdProviderPrivate)
 {
     if (const QString name = data.name(); !name.isEmpty()) {
-        d->name = name;
+        d_ptr->name = name;
     } else {
-        d->name = QStringLiteral("Unknown");
+        d_ptr->name = QStringLiteral("Unknown");
     }
 
     if (const QString identifier = data.value(QStringLiteral("X-KDE-PlasmaPoTDProvider-Identifier")); !identifier.isEmpty()) {
-        d->identifier = identifier;
+        d_ptr->identifier = identifier;
     } else {
-        d->identifier = d->name;
+        d_ptr->identifier = d_ptr->name;
     }
 
     if (!args.empty()) {
         for (const auto &arg : args) {
             const QDate date = QDate::fromString(arg.toString(), Qt::ISODate);
             if (date.isValid()) {
-                d->date = date;
+                d_ptr->date = date;
                 break;
             }
         }
     }
-
-    QString configFileName = d->identifier + QStringLiteral("provider.conf");
-    configRemoteUrl = QUrl(QStringLiteral(CONFIG_ROOT_URL) + configFileName);
-    configLocalPath = QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation) + QStringLiteral("/plasma_engine_potd/") + configFileName;
-    configLocalUrl = QUrl::fromLocalFile(configLocalPath);
-}
-
-PotdProvider::PotdProvider(QObject *parent, const QVariantList &args)
-    : QObject(parent)
-    , d(new PotdProviderPrivate)
-{
-    Q_UNUSED(args)
-    d->name = QStringLiteral("Unknown");
-    d->identifier = d->name;
-    qWarning() << "You are using an old PoTD provider plugin. It will not work in Plasma 6. Please consider updating the plugin.";
 }
 
 PotdProvider::~PotdProvider()
@@ -83,104 +59,43 @@ PotdProvider::~PotdProvider()
 
 QString PotdProvider::name() const
 {
+    Q_D(const PotdProvider);
     return d->name;
-}
-
-QDate PotdProvider::date() const
-{
-    return d->date.isNull() ? QDate::currentDate() : d->date;
-}
-
-bool PotdProvider::isFixedDate() const
-{
-    return !d->date.isNull();
 }
 
 QString PotdProvider::identifier() const
 {
+    Q_D(const PotdProvider);
     return d->identifier;
 }
 
-QImage PotdProvider::image() const
+QString PotdProvider::localPath() const
 {
-    return potdProviderData()->wallpaperImage;
+    return QString();
 }
 
 QUrl PotdProvider::remoteUrl() const
 {
-    return potdProviderData()->wallpaperRemoteUrl;
+    return m_remoteUrl;
 }
 
 QUrl PotdProvider::infoUrl() const
 {
-    return potdProviderData()->wallpaperInfoUrl;
+    return m_infoUrl;
 }
 
 QString PotdProvider::title() const
 {
-    return potdProviderData()->wallpaperTitle;
+    return m_title;
 }
 
 QString PotdProvider::author() const
 {
-    return potdProviderData()->wallpaperAuthor;
+    return m_author;
 }
 
-void PotdProvider::refreshConfig()
+QDate PotdProvider::date() const
 {
-    // You can only refresh it once in a provider's life cycle
-    if (refreshed) {
-        return;
-    }
-    // You can only refresh it once in a day
-    QFileInfo configFileInfo = QFileInfo(configLocalPath);
-    if (configFileInfo.exists() && configFileInfo.lastModified().addDays(1) > QDateTime::currentDateTime()) {
-        return;
-    }
-
-    KIO::StoredTransferJob *job = KIO::storedGet(configRemoteUrl, KIO::NoReload, KIO::HideProgressInfo);
-    connect(job, &KIO::StoredTransferJob::finished, this, &PotdProvider::configRequestFinished);
-
-    refreshed = true;
-}
-
-void PotdProvider::configRequestFinished(KJob *_job)
-{
-    KIO::StoredTransferJob *job = static_cast<KIO::StoredTransferJob *>(_job);
-    if (job->error()) {
-        Q_EMIT error(this);
-        qDebug() << "configRequestFinished error: failed to fetch data";
-        return;
-    }
-
-    KIO::StoredTransferJob *putJob = KIO::storedPut(job->data(), configLocalUrl, -1);
-    connect(putJob, &KIO::StoredTransferJob::finished, this, &PotdProvider::configWriteFinished);
-}
-
-void PotdProvider::configWriteFinished(KJob *_job)
-{
-    KIO::StoredTransferJob *job = static_cast<KIO::StoredTransferJob *>(_job);
-    if (job->error()) {
-        Q_EMIT error(this);
-        qDebug() << "configWriteFinished error: failed to write data";
-        return;
-    }
-
-    loadConfig();
-}
-
-void PotdProvider::loadConfig()
-{
-    KConfig config(configLocalPath);
-
-    KConfigGroup apiGroup = config.group("API");
-    QString apiKey = apiGroup.readEntry("API_KEY");
-    QString apiSecret = apiGroup.readEntry("API_SECRET");
-
-    Q_EMIT configLoaded(apiKey, apiSecret);
-}
-
-PotdProviderData *PotdProvider::potdProviderData() const
-{
-    return d->m_data.get();
+    Q_D(const PotdProvider);
+    return d->date.isNull() ? QDate::currentDate() : d->date;
 }
--- wallpapers/potd/plugins/potdprovider.h
+++ wallpapers/potd/plugins/potdprovider.h
@@ -5,29 +5,14 @@
 #pragma once
 
 #include <QImage>
-#include <QObject>
 #include <QUrl>
 #include <QVariantList>
 
-#include <KIO/Job>
 #include <KPluginMetaData>
 
 #include "plasma_potd_export.h"
 
 class QDate;
-
-/**
- * This class is used to store wallpaper data.
- */
-struct PotdProviderData {
-    QImage wallpaperImage;
-    QString wallpaperLocalUrl;
-    QUrl wallpaperRemoteUrl;
-    QUrl wallpaperInfoUrl;
-    QString wallpaperTitle;
-    QString wallpaperAuthor;
-};
-Q_DECLARE_METATYPE(PotdProviderData)
 
 /**
  * This class is an interface for PoTD providers.
@@ -43,26 +28,22 @@ public:
      * @param parent The parent object.
      * @param data The metadata of the plugin
      * @param args The arguments.
+     * @since 5.25
      */
     explicit PotdProvider(QObject *parent, const KPluginMetaData &data, const QVariantList &args);
 
     /**
-     * @deprecated Since 5.25. The constructor will be removed in Plasma 6.
-     */
-    PLASMA_POTD_DEPRECATED explicit PotdProvider(QObject *parent, const QVariantList &args);
-
-    /**
      * Destroys the PoTD provider.
      */
-    ~PotdProvider() override;
+    virtual ~PotdProvider() override;
 
     /**
-     * Returns the requested image.
+     * Returns the local path of the requested image.
      *
-     * Note: This method returns only a valid image after the
+     * Note: This method returns only a valid path after the
      *       finished() signal has been emitted.
      */
-    virtual QImage image() const;
+    virtual QString localPath() const;
 
     /**
      * Returns the identifier of the PoTD request (name + date).
@@ -76,7 +57,7 @@ public:
      * @return the remote URL of the image, if any
      * @since 5.25
      */
-    QUrl remoteUrl() const;
+    virtual QUrl remoteUrl() const;
 
     /**
      * Returns the information URL of the image from the provider
@@ -84,7 +65,7 @@ public:
      * @return the information URL of the image, if any
      * @since 5.25
      */
-    QUrl infoUrl() const;
+    virtual QUrl infoUrl() const;
 
     /**
      * Returns the title of the image from the provider, if any.
@@ -92,7 +73,7 @@ public:
      * @return the title of the image, if any
      * @since 5.25
      */
-    QString title() const;
+    virtual QString title() const;
 
     /**
      * Returns the author of the image from the provider
@@ -100,25 +81,17 @@ public:
      * @return the title of the image, if any
      * @since 5.25
      */
-    QString author() const;
+    virtual QString author() const;
 
     /**
      * @return the name of this provider (equiv to X-KDE-PlasmaPoTDProvider-Identifier)
      */
-    QString name() const;
+    virtual QString name() const;
 
     /**
      * @return the date to load for this item, if any
      */
-    QDate date() const;
-
-    /**
-     * @return if the date is fixed, or if it should always be "today"
-     */
-    bool isFixedDate() const;
-
-    void refreshConfig();
-    void loadConfig();
+    virtual QDate date() const;
 
 Q_SIGNALS:
     /**
@@ -126,8 +99,9 @@ Q_SIGNALS:
      * successfully.
      *
      * @param provider The provider which emitted the signal.
+     * @param image The image from the provider.
      */
-    void finished(PotdProvider *provider);
+    void finished(PotdProvider *provider, const QImage &image);
 
     /**
      * This signal is emitted whenever an error has occurred.
@@ -136,19 +110,14 @@ Q_SIGNALS:
      */
     void error(PotdProvider *provider);
 
-    void configLoaded(QString apiKey, QString apiSecret);
-
 protected:
-    PotdProviderData *potdProviderData() const;
+    std::unique_ptr<class PotdProviderPrivate> d_ptr;
+
+    QUrl m_remoteUrl;
+    QUrl m_infoUrl;
+    QString m_title;
+    QString m_author;
 
 private:
-    void configRequestFinished(KJob *job);
-    void configWriteFinished(KJob *job);
-
-    const QScopedPointer<class PotdProviderPrivate> d;
-
-    QUrl configRemoteUrl;
-    QUrl configLocalUrl;
-    QString configLocalPath;
-    bool refreshed = false;
+    Q_DECLARE_PRIVATE(PotdProvider)
 };
