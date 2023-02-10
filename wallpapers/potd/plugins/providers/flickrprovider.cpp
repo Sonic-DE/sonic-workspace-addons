faa7a9833f7588d348751fe959f0310f14356532
Fushan Wen <qydwhotmail@gmail.com>
2023-02-10 22:48:20 +0800

    wallpapers/potd: use categorized logging in providers

--- wallpapers/potd/plugins/providers/flickrprovider.cpp
+++ wallpapers/potd/plugins/providers/flickrprovider.cpp
@@ -19,6 +19,8 @@
 #include <KPluginFactory>
 #include <KSharedConfig>
 
+#include "debug.h"
+
 #define CONFIG_ROOT_URL "https://autoconfig.kde.org/potd/"
 
 static QUrl buildUrl(const QDate &date, const QString &apiKey)
@@ -47,7 +49,7 @@ void FlickrProvider::configRequestFinished(KJob *_job)
 {
     KIO::StoredTransferJob *job = static_cast<KIO::StoredTransferJob *>(_job);
     if (job->error()) {
-        qWarning() << QStringLiteral("configRequestFinished error: failed to fetch data");
+        qCWarning(WALLPAPERPOTD) << QStringLiteral("configRequestFinished error: failed to fetch data");
         Q_EMIT error(this);
         return;
     }
@@ -60,7 +62,7 @@ void FlickrProvider::configWriteFinished(KJob *_job)
 {
     KIO::StoredTransferJob *job = static_cast<KIO::StoredTransferJob *>(_job);
     if (job->error()) {
-        qWarning() << QStringLiteral("configWriteFinished error: failed to write data. %1").arg(job->errorText());
+        qCWarning(WALLPAPERPOTD) << QStringLiteral("configWriteFinished error: failed to write data. %1").arg(job->errorText());
         Q_EMIT error(this);
         return;
     }
@@ -122,7 +124,7 @@ void FlickrProvider::xmlRequestFinished(KJob *_job)
 {
     KIO::StoredTransferJob *job = static_cast<KIO::StoredTransferJob *>(_job);
     if (job->error()) {
-        qWarning() << QStringLiteral("XML request error: %1").arg(job->errorText());
+        qCWarning(WALLPAPERPOTD) << QStringLiteral("XML request error: %1").arg(job->errorText());
         Q_EMIT error(this);
         return;
     }
@@ -144,7 +146,7 @@ void FlickrProvider::xmlRequestFinished(KJob *_job)
             if (xml.name() == QLatin1String("rsp")) {
                 /* no pictures available for the specified parameters */
                 if (attributes.value(QLatin1String("stat")).toString() != QLatin1String("ok")) {
-                    qWarning() << QStringLiteral("xmlRequestFinished error: no photos for the query");
+                    qCWarning(WALLPAPERPOTD) << QStringLiteral("xmlRequestFinished error: no photos for the query");
                     Q_EMIT error(this);
                     return;
                 }
@@ -193,7 +195,7 @@ void FlickrProvider::xmlRequestFinished(KJob *_job)
     }
 
     if (xml.error() && xml.error() != QXmlStreamReader::PrematureEndOfDocumentError) {
-        qWarning() << QStringLiteral("XML ERROR at line %1: %2").arg(xml.lineNumber(), xml.error());
+        qCWarning(WALLPAPERPOTD) << QStringLiteral("XML ERROR at line %1: %2").arg(xml.lineNumber(), xml.error());
     }
 
     if (m_photoList.begin() != m_photoList.end()) {
@@ -217,7 +219,7 @@ void FlickrProvider::xmlRequestFinished(KJob *_job)
         KIO::StoredTransferJob *imageJob = KIO::storedGet(m_remoteUrl, KIO::NoReload, KIO::HideProgressInfo);
         connect(imageJob, &KIO::StoredTransferJob::finished, this, &FlickrProvider::imageRequestFinished);
     } else {
-        qWarning() << QStringLiteral("List is empty in XML file");
+        qCWarning(WALLPAPERPOTD) << QStringLiteral("List is empty in XML file");
         Q_EMIT error(this);
     }
 }
@@ -226,7 +228,7 @@ void FlickrProvider::imageRequestFinished(KJob *_job)
 {
     KIO::StoredTransferJob *job = static_cast<KIO::StoredTransferJob *>(_job);
     if (job->error()) {
-        qWarning() << QStringLiteral("Image request error: %1").arg(job->errorText());
+        qCWarning(WALLPAPERPOTD) << QStringLiteral("Image request error: %1").arg(job->errorText());
         Q_EMIT error(this);
         return;
     }
@@ -247,7 +249,7 @@ void FlickrProvider::pageRequestFinished(KJob *_job)
 {
     KIO::StoredTransferJob *job = static_cast<KIO::StoredTransferJob *>(_job);
     if (job->error()) {
-        qWarning() << QStringLiteral("No author available");
+        qCWarning(WALLPAPERPOTD) << QStringLiteral("No author available");
         Q_EMIT finished(this, m_image);
         return;
     }
