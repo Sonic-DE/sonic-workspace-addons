2e27041b0082d3e31f5a35706b8d6aa0cabee075
Fushan Wen <qydwhotmail@gmail.com>
2023-12-24 21:55:38 +0800

    wallpapers/potd: fix wallpaper not being updated
    The imageChanged signal in the backend was not used, so when the update
    process finishes, the current image was not updated.

--- wallpapers/potd/plugins/potdbackend.cpp
+++ wallpapers/potd/plugins/potdbackend.cpp
@@ -265,6 +265,7 @@ void PotdBackend::registerClient()
     connect(m_client, &PotdClient::remoteUrlChanged, this, &PotdBackend::remoteUrlChanged);
     connect(m_client, &PotdClient::titleChanged, this, &PotdBackend::titleChanged);
     connect(m_client, &PotdClient::authorChanged, this, &PotdBackend::authorChanged);
+    connect(m_client, &PotdClient::done, this, &PotdBackend::imageChanged);
 
     // Refresh the desktop wallpaper and the information in config dialog
     Q_EMIT loadingChanged();
