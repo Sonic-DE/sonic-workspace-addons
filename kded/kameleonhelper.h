5611896aec70004fc7c7a4afa9b2ca7439ab8f5b
Natalie Clarius <natalie_clarius@yahoo.de>
2024-03-17 01:29:07 +0100

    add kded module to sync device LEDs with accent color

---
+++ kded/kameleonhelper.h
@@ -0,0 +1,23 @@
+/*
+    SPDX-FileCopyrightText: 2013 Valentin Rusu <kde@rusu.info>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#ifndef _KAMELEONHELPER_H_
+#define _KAMELEONHELPER_H_
+
+#include <QObject>
+
+#include <KAuth/ActionReply>
+
+using namespace KAuth;
+
+class KameleonHelper : public QObject
+{
+    Q_OBJECT
+public Q_SLOTS:
+    KAuth::ActionReply writecolor(const QVariantMap &args);
+};
+
+#endif // _KameleonHELPER_H_
