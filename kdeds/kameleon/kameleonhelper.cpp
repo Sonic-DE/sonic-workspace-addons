ad59901a9077c71458f0ec1099cad8fe83395c61
Natalie Clarius <natalie_clarius@yahoo.de>
2024-03-17 01:29:07 +0100

    add kded module to sync device LEDs with accent color

---
+++ kdeds/kameleon/kameleonhelper.cpp
@@ -0,0 +1,56 @@
+/*
+    SPDX-FileCopyrightText: 2024 Natalie Clarius <natalie.clarius@kde.org>
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
+#include "kameleonhelper.h"
+#include "kameleonhelper_debug.h"
+
+#include <KAuth/HelperSupport>
+
+#include <QDebug>
+#include <QFile>
+#include <QFileInfo>
+
+KAuth::ActionReply KameleonHelper::writecolor(const QVariantMap &args)
+{
+    QMap<QString, QVariant> devices = args.value(QStringLiteral("entries")).toMap();
+    for (auto i = devices.cbegin(), end = devices.cend(); i != end; ++i) {
+        QString device = i.key();
+        QByteArray color = i.value().toByteArray();
+        if (!(color.length() >= QByteArray("0 0 0").length() && color.length() <= QByteArray("255 255 255").length())) {
+            qCWarning(KAMELEONHELPER) << "invalid RGB color" << color << "for device" << devices;
+            return KAuth::ActionReply::HelperErrorReply();
+        }
+
+        QFile file(LED_SYSFS_PATH + device + LED_RGB_FILE);
+        if (!QFileInfo(file).exists()) {
+            qCWarning(KAMELEONHELPER) << "writing to" << file.fileName() << "failed:"
+                                      << "file does not exist";
+            return KAuth::ActionReply::HelperErrorReply();
+        }
+        if (!QFileInfo(file).isWritable()) {
+            qCWarning(KAMELEONHELPER) << "writing to" << file.fileName() << "failed:"
+                                      << "file is not writable";
+            return KAuth::ActionReply::HelperErrorReply();
+        }
+        if (!file.open(QIODevice::WriteOnly)) {
+            qCWarning(KAMELEONHELPER) << "writing to" << file.fileName() << "failed:" << file.error() << file.errorString();
+            return KAuth::ActionReply::HelperErrorReply();
+        }
+        const int bytesWritten = file.write(color);
+        if (bytesWritten == -1) {
+            qCWarning(KAMELEONHELPER) << "writing to" << file.fileName() << "failed:" << file.error() << file.errorString();
+            return KAuth::ActionReply::HelperErrorReply();
+        } else {
+            qCInfo(KAMELEONHELPER) << "wrote color" << color << "to" << file.fileName();
+        }
+    }
+
+    qCInfo(KAMELEONHELPER) << "wrote color" << color << "to LED devices";
+    return KAuth::ActionReply::SuccessReply();
+}
+
+KAUTH_HELPER_MAIN("org.kde.kameleonhelper", KameleonHelper)
+
+#include "moc_kameleonhelper.cpp"
