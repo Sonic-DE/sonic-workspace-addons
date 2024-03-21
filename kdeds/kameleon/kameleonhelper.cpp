cf860ec5aee0ff8bf845e839aca8e4da7201c447
Natalie Clarius <natalie_clarius@yahoo.de>
2024-03-21 03:03:30 +0100

    kdeds/kameleon: add support for setting custom color
    As an alternative to following the accent color.

--- kdeds/kameleon/kameleonhelper.cpp
+++ kdeds/kameleon/kameleonhelper.cpp
@@ -14,12 +14,18 @@
 
 KAuth::ActionReply KameleonHelper::writecolor(const QVariantMap &args)
 {
-    QMap<QString, QVariant> devices = args.value(QStringLiteral("entries")).toMap();
-    for (auto i = devices.cbegin(), end = devices.cend(); i != end; ++i) {
-        QString device = i.key();
-        QByteArray color = i.value().toByteArray();
+    QStringList devices = args.value(QStringLiteral("devices")).toStringList();
+    QStringList colors = args.value(QStringLiteral("colors")).toStringList();
+    if (devices.length() != colors.length()) {
+        qCWarning(KAMELEONHELPER) << "lists of devices and colors do not match in length";
+        return KAuth::ActionReply::HelperErrorReply();
+    }
+
+    for (int i = 0; i < devices.length(); ++i) {
+        QString device = devices.at(i);
+        QByteArray color = colors.at(i).toUtf8();
         if (!(color.length() >= QByteArray("0 0 0").length() && color.length() <= QByteArray("255 255 255").length())) {
-            qCWarning(KAMELEONHELPER) << "invalid RGB color" << color << "for device" << devices;
+            qCWarning(KAMELEONHELPER) << "invalid RGB color" << color << "for device" << device;
             return KAuth::ActionReply::HelperErrorReply();
         }
 
@@ -42,12 +48,9 @@ KAuth::ActionReply KameleonHelper::writecolor(const QVariantMap &args)
         if (bytesWritten == -1) {
             qCWarning(KAMELEONHELPER) << "writing to" << file.fileName() << "failed:" << file.error() << file.errorString();
             return KAuth::ActionReply::HelperErrorReply();
-        } else {
-            qCInfo(KAMELEONHELPER) << "wrote color" << color << "to" << file.fileName();
         }
     }
 
-    qCInfo(KAMELEONHELPER) << "wrote color" << color << "to LED devices";
     return KAuth::ActionReply::SuccessReply();
 }
 
